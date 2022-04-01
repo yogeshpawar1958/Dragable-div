4a8b10accdec26158bf9a36738c3de93636cc6f7
gokloud-ypawar <yogesh.pawar@gokloud.io>
2022-04-01 11:17:22 +0530

    added gragable div

---
+++ src/Dragable.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import Draggable from 'react-draggable';
+
+const Dragable = () => {
+    return (
+        <>
+            <Draggable>
+                <div className="box">
+                    <div className="row">
+                        <div className="col-6 first">
+                            <div className='outer  '>
+                                <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, culpa nam?</p>
+                            </div>
+                        </div>
+
+                        <div className="col-6">
+                            <div className='second outer position-absolute top-50 start-50 '>
+                                <p className='text-center '>Lorem ipsum dolor sit amet </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Draggable>
+        </>
+    )
+}
+
+export default Dragable
